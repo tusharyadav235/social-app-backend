ebcc092caf31f9a4745c1d571a666915baacda30
tusharyadav235 <tusharforwork00@gmail.com>
2026-02-16 13:17:13 +0530

    Update JwtUtil.java

--- src/main/java/com/socialmedia/social_app/security/JwtUtil.java
+++ src/main/java/com/socialmedia/social_app/security/JwtUtil.java
@@ -13,7 +13,7 @@ import java.util.Date;
 @Component
 public class JwtUtil {
 
-    @Value("${jwt.secret:${JWT_SECRET}}")
+    @Value("${jwt.secret}")
     private String secret;
 
     private SecretKey getKey() {
